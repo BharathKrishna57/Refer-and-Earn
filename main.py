ee4cbfc191c8105b8f2c6f2875bbd5d31addf263
BharathKrishna57 <bharath@vridhihomefinance.com>
2025-08-11 19:33:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
- 
-from app.config import FRONTEND_URL
-from app.db.session import engine
-from app.models.vfh_models import Base
-from app.api.refer_router import router as refer_router
-from app.api.pincode import router as pincode
+
+from config import FRONTEND_URL
+from db.session import engine
+from models.vfh_models import Base
+from api.refer_router import router as refer_router
+from api.pincode import router as pincode
+
 # Load environment variables from .env file
 load_dotenv()
- 
+
 # Initialize database tables (only if needed)
 Base.metadata.create_all(bind=engine)
- 
+
 # Initialize FastAPI app
 app = FastAPI(title="Refer-A-Friend API")
- 
+
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
@@ -25,8 +26,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
- 
-# Register API router
+
+# Register API routers
 app.include_router(refer_router)
- 
 app.include_router(pincode, prefix="/api")
