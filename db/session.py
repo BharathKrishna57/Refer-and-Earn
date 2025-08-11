6ea211f4585e0994b299a2d038d4c630f9c41a46
BharathKrishna57 <bharath@vridhihomefinance.com>
2025-08-11 20:43:33 +0530

    Update session.py

--- db/session.py
+++ db/session.py
@@ -2,10 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Use the remote freesqldatabase.com connection string
 DATABASE_URL = "mysql+pymysql://sql12794254:AB8FAY5dPj@sql12.freesqldatabase.com:3306/sql12794254"
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=0
+)
 
 SessionLocal = sessionmaker(bind=engine)
 
